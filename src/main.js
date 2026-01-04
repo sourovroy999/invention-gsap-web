db38f4888c44e0b683e6301a3e0e7cbc363296d3
sourovroy999 <sourovroy3050@gmail.com>
2026-01-04 17:10:39 +0600

    general video section with scrolltrigger added

---
+++ src/main.js
@@ -0,0 +1,74 @@
+import gsap from 'gsap'
+import './style.css'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+// gsap.set('#first-video', {
+//   height:'300px',
+//   width:'300px'
+// })
+
+gsap.set('#first-video', {
+  transformPerspective:500,
+  rotateX:10,
+  // scale:1.5,
+
+  // rotateY:10,
+})
+
+
+
+
+gsap.to('#first-video', {
+  // scale:5,
+    // start:'top top',
+    rotateX:0,
+    ease:'power1.inOut',
+
+    // rotateY:0,
+
+  height:'100%',
+  width:'100%',
+  borderRadius:0,
+  // height:'800px',
+  // width:'1600px',
+  scrollTrigger:{
+    trigger:'#hero',
+    pin:true,
+    scrub:true,
+    invalidateOnRefresh: true,
+  
+
+  }
+})
+
+gsap.set('#test-red',{
+  transformPerspective:800,
+
+  rotateX:30,
+})
+
+gsap.to('#test-red', {
+  rotateX:0,
+  borderRadius:0,
+  // scale:5,
+    // start:'top top',
+
+    ease:'power1.out',
+
+  height:'100%',
+  width:'100%',
+  // height:'800px',
+  // width:'1600px',
+  scrollTrigger:{
+    trigger:'#test-hero',
+    pin:true,
+    scrub:true,
+    invalidateOnRefresh: true,
+    
+    // pinSpacing:false,
+  
+
+  }
+})
