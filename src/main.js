7b72bd66a21e322a405292304e47a7e5eafe6872
sourovroy999 <sourovroy3050@gmail.com>
2026-01-04 19:08:08 +0600

    layered pinning added

--- src/main.js
+++ src/main.js
@@ -37,38 +37,66 @@ gsap.to('#first-video', {
     trigger:'#hero',
     pin:true,
     scrub:true,
-    invalidateOnRefresh: true,
+  
   
 
   }
 })
 
-gsap.set('#test-red',{
-  transformPerspective:800,
+// gsap.set('#test-red',{
+//   transformPerspective:800,
 
-  rotateX:30,
-})
+//   rotateX:30,
+// })
 
-gsap.to('#test-red', {
-  rotateX:0,
-  borderRadius:0,
-  // scale:5,
-    // start:'top top',
+// gsap.to('#test-red', {
+//   rotateX:0,
+//   borderRadius:0,
+//   // scale:5,
+//     // start:'top top',
 
-    ease:'power1.out',
+//     ease:'power1.out',
 
-  height:'100%',
-  width:'100%',
-  // height:'800px',
-  // width:'1600px',
-  scrollTrigger:{
-    trigger:'#test-hero',
-    pin:true,
-    scrub:true,
-    invalidateOnRefresh: true,
+//   height:'100%',
+//   width:'100%',
+//   // height:'800px',
+//   // width:'1600px',
+//   scrollTrigger:{
+//     trigger:'#test-hero',
+//     pin:true,
+//     scrub:true,
+  
     
-    // pinSpacing:false,
+//     // pinSpacing:false,
   
 
+//   }
+// })
+
+
+
+
+gsap.to('.panel:not(:last-child)', {
+  yPercent:-100,
+  ease:'none',
+  stagger:0.5,
+
+  scrollTrigger:{
+    trigger:'#pinning-container',
+    start:'top top',
+    end:'+=200%',
+    scrub:true,
+    pin:true,
   }
+
 })
+
+gsap.set('#panel',{
+  zIndex: (i, target, targets)=> targets.length-i
+});
+
+
+
+
+
+
